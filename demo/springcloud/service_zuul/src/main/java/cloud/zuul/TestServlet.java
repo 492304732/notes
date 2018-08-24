c0033e223f511148e53e4a5b1c02b9c29e5b4519
guosujin <492304732@qq.com>
2018-08-24 17:36:47 +0800

    springcloud demo

---
+++ demo/springcloud/service_zuul/src/main/java/cloud/zuul/TestServlet.java
@@ -0,0 +1,23 @@
+package cloud.zuul;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @Description: TODO
+ * @author: 01369674
+ * @date: 2018/8/23
+ */
+public class TestServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+}
